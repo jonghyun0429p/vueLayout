eba779fbb2a8258bfcc437d9c14b8cadc1487cb8
jonghyun0429p <bwhdgus0429@naver.com>
2022-04-09 19:52:43 +0900

    레이아웃

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>div 요소를 이요한 레이아웃</h1>
+ <div id="header">
+   <h2>Header 영역</h2>
+  </div>
+  <div id="nav">
+    <h2>NAV 영역</h2>
+  </div>
+  <div id="section">
+    <p>SECTION 영역</p>
+  </div>
+  <div id="footer">
+    <h2>FOOTER 영역</h2>
+  </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+
+
+
+
 </script>
 
 <style>
@@ -23,4 +34,33 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#header {
+  background-color : lightgrey;
+  height: 300px;
+}
+
+#nav {
+  background-color : #339999;
+  color:white;
+  width:200px;
+  float:left;
+}
+
+#section {
+  width:200px;
+  text-align:left;
+  float:left;
+  padding:5px;
+}
+
+#footer {
+  background-color:#FFCC00;
+  height:300px;
+  clear:both;
+}
+
+#header, #nav, #section, #footer { text-align:center; }
+#header, #footer { line-height:300px; }
+#nav, #section { line-height:2000px; }
 </style>
